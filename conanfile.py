51450d51cb95403ec122961f9f83a4f3c50b04fd
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:17:11 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,22 @@ class BoostCoroutine2Conan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/coroutine2"
-    description = "For a description of this library, please visit http://boost.org/coroutine2 "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "coroutine2"
-
+    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Context/1.64.0@bincrafters/testing"
+                      
+                      #assert1 config0 context12
+                      
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
