46d96618b89f2265070c0ce76a1122bb0aaf2a2f
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:09 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostCoroutine2Conan(ConanFile):
     name = "Boost.Coroutine2"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/coroutine2"
+    url = "https://github.com/bincrafters/conan-boost-coroutine2"
+    source_url = "https://github.com/boostorg/coroutine2"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "coroutine2"
@@ -16,7 +16,7 @@ class BoostCoroutine2Conan(ConanFile):
                       
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
