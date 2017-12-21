cae6cbc7664a7eef83d7e92911d2fd9f8ff91c89
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:31 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,20 +3,20 @@ from conans import ConanFile
 
 class BoostCoroutine2Conan(ConanFile):
     name = "Boost.Coroutine2"
-    version = "1.65.1"
+    version = "1.66.0"
     requires = \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Context/1.65.1@bincrafters/testing"
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Context/1.66.0@bincrafters/testing"
     lib_short_names = ["coroutine2"]
     is_header_only = True
 
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-coroutine2"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
